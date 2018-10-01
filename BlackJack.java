b9ccbf76caf6ee8a6a90bce9659c70937f70a052
Devoid <42281396+Rubikscrafter@users.noreply.github.com>
2018-10-01 13:00:42 -0400

    Update BlackJack.java

--- BlackJack.java
+++ BlackJack.java
@@ -10,5 +10,21 @@ public class Blackjack {
     System.out.println("Face cards are worth 10, and aces are worth either 1 or 11.:);
     System.out.println('Enter the word "Draw" to begin');
     String move = keyboard.nextLine();
+                       
+    if (move.equalsIgnoreCase("draw") {
+      /* Make the card variable, which is random, and if the random number is 10, 
+      then randomly choose between the 10 card or a face card. 
+      Figure out if you can make the 1 and 11 values for ace the same variable, 
+      so that there is an equal chance of getting either one. */
+      
+      System.out.println("Your first draw is: +card");
+    
+    } else if (move.equalsIgnoreCase("quit") {
+      System.out.println("Thank you for playing.");
+      break;
+    } else {
+      System.out.println("Please enter "Draw" to begin");
+      move = keyboard.nextLine();
+    }
   }
 }
