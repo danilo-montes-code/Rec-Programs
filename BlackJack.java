5c80a33bc9ce367ea7d5ecc4189fa6ccccdaa0bb
Devoid <42281396+Rubikscrafter@users.noreply.github.com>
2018-10-11 22:13:22 -0400

    Update BlackJack.java

--- BlackJack.java
+++ BlackJack.java
@@ -1,97 +1,83 @@
 import java.util.Scanner;
 import java.util.Random;
-//Add blackjack ai somehow
-//Add date for randomness
+//TODO Add blackjack ai somehow
+//TODO Maybe add playing again function
 public class Blackjack {
-  static int Random = new Random();
-  static Scanner keyboard = new Scanner(System.in);
-  
-  public void playTheGame() {
-    int card = rng.nextInt(13)+1;
-    String faces = " JQK";
-    String cardFace = "";
-    int playerTotal = 0;
-    
+  static Random rng = new Random(); //Rng
+  static Scanner keyboard = new Scanner(System.in); //Input
+  static int card = rng.nextInt(13)+1;
+  static int playerTotal = 0;
+  static String playing = ""; //Used for in game check to see if the user stands
+  static String[] Suits = {
+    "♠",
+    "♥",
+    "♦",
+    "♣",
+  };
+  static String[] Cards = {
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  };
+
+  public static void playTheGame() {
+    System.out.println("Enter 'draw' to draw a card and 'stand' to stop receiving cards.");
+    playing = keyboard.nextLine();
+    switch (playing){
+      //If you draw
+      case "draw":
+      case "Draw":
+
+      break;
+      //If you quit
+      case "stand":
+      case "Stand":
+      break;
+    }
+  } //End of playing the game
+
+  public static void main (String [] args) {
     System.out.println("Welcome to Blackjack");
     System.out.println("The goal is to get as close as you can to the number 21 without going over.");
     System.out.println("Face cards are worth 10, and aces are worth either 1 or 11.");
-    System.out.println("Enter the word 'Draw' to begin, and the word 'Quit' to quit (you can quit at any time).");
-    String move = keyboard.nextLine();
-    
-    if (move.equalsIgnoreCase("draw")) {
-      while(playerTotal <= 21) {
-        if (card > 10){ //Face cards
-          cardFace = faces.substring(card, card+1);
-          System.out.println("Your card is a "+cardFace);
-          playerTotal = playerTotal + 10;
-          System.out.println("Your current total is "+playerTotal);
-          System.out.println("Would you like to draw another card?");
-          move = keyboard.nextLine();
-        } else if (card > 1 && card <= 10) {
-          System.out.println("Your card is "+card);
-          playerTotal = playerTotal + card;
-          System.out.println("Your current total is "+playerTotal);
-          System.out.println("Would you like to draw another card?");
-          move = keyboard.nextLine();
-        } else if (card == 1) {
-          System.out.println("Your card is an ace.");
-          if (playerTotal+11 > 21 && playerTotal+1 > 21) {
-            playerTotal = playerTotal + card;
-            System.out.println("You busted! Enter 'yes' to try again and 'quit' to stop.");
-            move = keyboard.nextLine();
-          } else if (playerTotal + 1 <+ 21) {
-            playerTotal = playerTotal + card;
-            System.out.println("Your current total is "+playerTotal);
-            System.out.println("Would you like to draw another card?");
-            move = keyboard.nextLine();
-          }
-        }
-        card = rng.nextInt(13)+1;
-        System.out.println("You won! You card total is "+playerTotal);
-        System.out.println("Enter 'draw' to play again and 'quit' to quit.");
-        move = keyboard.nextLine();
-        /* 
-         Make it so that numbers 1 to 13 are possibilities, 11 means J, 12 means Q, 13 means K, and 1 nets you an ace.
-         
-         Also, nested if if the player gets an ace so that the 1 value can be counted
-         if the 11 puts the player over 11. Something like
-         
-         if (11+playerTotal > 21)
-         playerTotal = playerTotal-10;
-         }
-         */
-        
-        
+    boolean game = true;
+    boolean win = true;
+    int numOfCards = 1;
+
+    while (game == true) {
+      playTheGame();
+      if (playerTotal > 21) {
+        win = false;
+        game = false;
+        System.out.println("Your final total is "+playerTotal" with "+numOfCards+" cards drawn.");
       }
-    } else if (move.equalsIgnoreCase("quit")) {
-      System.out.println("Thank you for playing.");
-      playerTotal = 100;
-    } else {
-      System.out.println("Please enter 'Draw' to begin");
-      move = keyboard.nextLine();
-    }
-  } //End of playing the game
-  
-  
-  
-  public static void main (String [] args) {
-    Scanner keyboard = new Scanner(System.in); // delete when static scanner is made
-    Random rng = new Random(); // add under static scanner
-    String answer;
-    playTheGame();
-    System.out.println("Would you like to play again?");
-    answer = keyboard.nextLine();
-    while (!answer.equalsIgnoreCase("yes") || !answer.equalsIgnoreCase("no")) {
-      if (answer.equalsIgnoreCase("yes")) {
-        playTheGame();
-      } else if (answer.equalsIgnoreCase("no")) {
-        System.out.println("Thanks for playing!");
-      } else {
-        System.out.println("Please enter either 'yes' or 'no'.");
-        answer = keyboard.nextLine();
+      if (playing.equalsIgnoreCase("stand")) { //Once Ai is added, check if playerTotal > aiTotal and give win accordingly
+        game = false;
+        System.out.println("Your final total is "+playerTotal" with "+numOfCards+" cards drawn.");
       }
-    }
+      if (game == true) {
+        System.out.println("Your current total is "+playerTotal+" with "+numOfCards+" cards drawn.");
+        cards++;
+      }
+    } //End of playing check
+    System.out.println("Thank you for playing!");
   } //End of main
 } //End of class
 
-//https://github.com/TomFanella4/Blackjack-Card-Game-with-AI/blob/master/Blackjack.java
+/*TODO Nested if if the player gets an ace so that the 1 value can be counted
+if the 11 puts the player over 11. Something like
+
+if (11+playerTotal > 21)
+playerTotal = playerTotal-10;
+*/
+//https://github.com/TomFanella4/Blackjack-Card-Game-with-AI/blob/master/Blackjack.java (If I need a guide)
