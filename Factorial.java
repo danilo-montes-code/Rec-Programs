ad3ebd10ff687da32466106418d4907087ba80d5
Devoid <42281396+Rubikscrafter@users.noreply.github.com>
2018-10-12 23:29:23 -0400

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -5,9 +5,9 @@ public class Factorial {
     Scanner keyboard = new Scanner(System.in);
     
     System.out.println("Enter Number");
-    int number = keyboard.nextInt();
-    int ognum = number;
-    for(int x = number-1; x>=1; x--) {
+    long number = keyboard.nextInt();
+    long ognum = number;
+    for(long x = number-1; x>=1; x--) {
       number = x*number;
     }
     System.out.println(ognum+"! is "+number);
