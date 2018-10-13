a81a83b1c28ced7a5db5b55fcfcf7941516a7450
Devoid <42281396+Rubikscrafter@users.noreply.github.com>
2018-10-13 00:12:28 -0400

    Update BlackJack.java

--- BlackJack.java
+++ BlackJack.java
@@ -1,78 +1,42 @@
 import java.util.Scanner;
 import java.util.Random;
+import java.util.ArrayList;
+import java.util.Collections;
 //TODO Add blackjack ai somehow
 //TODO Maybe add playing again function
 public class Blackjack {
   static Random rng = new Random(); //Rng
+  static int cardPicker = rng.nextInt(52); //Card rng
   static Scanner keyboard = new Scanner(System.in); //Input
-  static int card = rng.nextInt(13);
-  static int playerTotal = 0;
+  static int playerTotal = 0; //User's in game total
   static String playing; //Used for in game check to see if the user stands
-  static String[] suits = {
-    "?",
-    "?",
-    "?",
-    "?",
-  };
-  static String[] cards = {
-    "A",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J",
-    "Q",
-    "K"
-  };
+  static int turn = 1; //Turn counter
+  static int numOfCards = 0; //Draw counter
+  //Deck stuff
+  static String[] suitsArray = {"♠","♥","♦","♣",};
+  static String[] cardsArray = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+  static ArrayList<String> deck = new ArrayList<String>(); //Deck
+  static ArrayList<String> suits = new ArrayList<String>(); //Suits
+  static ArrayList<String> cards = new ArrayList<String>(); //Cards
+  static ArrayList<String> playerDraw = new ArrayList<String>();
 
-  public static void playTheGame() {
-    System.out.println("Enter 'draw' to draw a card and 'stand' to stop receiving cards.");
-    playing = keyboard.nextLine();
-    while (!playing.equalsIgnoreCase("draw") && !playing.equalsIgnoreCase("stand")) {
-      System.out.println("Please enter a valid move.");
-      playing = keyboard.nextLine();
-    }
-    switch(playing) {
-      //If you draw
-      case "draw":
-      case "Draw":
-      if (card == 0 || card == 7) {
-        System.out.println("You drew an "+ cards[card]);
-      } else {
-        System.out.println("You drew a "+ cards[card]);
+  public static void shuffleDeck() { //Creates deck TODO make it shuffle instead of just create the deck
+    Collections.addAll(suits,suitsArray);
+    Collections.addAll(cards,cardsArray);
+    for (int suit = 0; suit<= 3; suit++) {
+      for (int card = 0; card <=12;card++) {
+        deck.add(cards.get(card)+suits.get(suit));
       }
-      if (card >=1 && card <=9) {
-        playerTotal = playerTotal + (card+1);
-      } else if (card >= 10) {
-        playerTotal = playerTotal + 10;
-      } else if (card == 0) { //TODO if playerTotal doesn't go over 21 when the ace is taken as 1 instead of 11 then do that please
-        if (playerTotal+11 > 21) {
-          playerTotal = playerTotal + 1;
-        } else if (playerTotal+11 <= 21) {
-          playerTotal = playerTotal + 11;
-        }
-      }
-      card = rng.nextInt(13); //TODO change rng stuff to make more rng
-      break;
-      //If you quit
-      case "stand":
-      case "Stand":
-      break;
     }
-  } //End of playing the game
+  } //End of shuffleDeck
 
   public static void main (String [] args) {
+    shuffleDeck(); //Currently only creates the deck in the same order everytime
     System.out.println("Welcome to Blackjack");
     System.out.println("The goal is to get as close as you can to the number 21 without going over.");
     System.out.println("Face cards are worth 10, and aces are worth either 1 or 11.");
     boolean game = true;
     boolean win = true;
-    int numOfCards = 1;
 
     while (game == true) {
       playTheGame();
@@ -89,7 +53,6 @@ public class Blackjack {
       }
       if (game == true) {
         System.out.println("Your current total is "+playerTotal+" with "+numOfCards+" cards drawn.");
-        numOfCards++;
       }
     } //End of playing check
     if (win == true) {
@@ -99,6 +62,72 @@ public class Blackjack {
     }
     System.out.println("Thank you for playing!");
   } //End of main
+
+  public static void playTheGame() {
+    System.out.println("Enter 'draw' to draw a card, 'stand' to stop receiving cards, and 'view' to see all the cards that you have drawn.");
+    playing = keyboard.nextLine();
+    while (!playing.equalsIgnoreCase("draw") && !playing.equalsIgnoreCase("stand") && !playing.equalsIgnoreCase("view")) {
+      System.out.println("Please enter a valid move.");
+      playing = keyboard.nextLine();
+    }
+    switch(playing) {
+      //If you draw
+      case "draw":
+      case "Draw": //TODO If at any point you drew an ace, the ace can become a 1 insetad of an 11 if needed
+      System.out.println("Turn "+turn);
+      turn++;
+      numOfCards++;
+      int cardVal = cardPicker();
+      if (cardVal == 1) {
+        if (playerTotal+11 > 21) {
+          cardVal = 1;
+        } else if (playerTotal+11 <= 21) {
+          cardVal = 11;
+        }
+      }
+      playerTotal = playerTotal + cardVal;
+      cardPicker = rng.nextInt(52); //TODO change rng stuff to make more rng
+      break;
+      //If you quit
+      case "stand":
+      case "Stand":
+      break;
+      case "view":
+      case "View":
+      if (turn == 1) {
+        System.out.println("You have not drawn any cards yet.");
+      } else {
+        System.out.println("So far, you have drawn: "+playerDraw);
+      }
+      break;
+    }
+  } //End of playTheGame
+  public static int cardPicker() {
+    String valString = deck.get(cardPicker);
+    System.out.println("You drew the "+valString);
+    playerDraw.add(valString);
+    String cardStringVal = valString.substring(0,valString.length()-3);
+    int cardVal = 0;
+    if (isNumeric(cardStringVal) == false) {
+      if (cardStringVal.equals("A")) {
+        cardVal = 1;
+      } else {
+        cardVal = 10;
+      }
+    } else {
+      cardVal = Integer.parseInt(cardStringVal);
+    }
+    return cardVal;
+  } //End of cardPicker
+
+  public static boolean isNumeric(String strNum) {
+    try {
+      int d = Integer.parseInt(strNum);
+    } catch (NumberFormatException | NullPointerException nfe) {
+      return false;
+    }
+    return true;
+  } //End of isNumeric
 } //End of class
 
 /*TODO Nested if if the player gets an ace so that the 1 value can be counted
