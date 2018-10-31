bbf5046ba66f4babbb18085b6eb99de92cc8b56b
Devoid <42281396+Rubikscrafter@users.noreply.github.com>
2018-10-31 19:58:20 -0400

    Delete Cards.java

--- BlackJack/FinalBlackJack/Cards.java
+++
@@ -1,9 +0,0 @@
-public class Cards {
-    private String card;
-    private String suit;
-
-    public Cards (String card, String suit){
-        this.card = card;
-        this.suit = suit;
-    }
-}
