e6ac68a761804c164b2cf293629289e7d0eb091d
Devoid <42281396+Rubikscrafter@users.noreply.github.com>
2018-10-31 23:42:27 -0400

    Add files via upload

--- BlackJack/FinalBlackJack/BlackJackGame.java
+++ BlackJack/FinalBlackJack/BlackJackGame.java
@@ -1,8 +1,18 @@
-public class BlackJackGame {
-    public BlackJackGame() {
-        Decks deck = new Decks();
-        deck.deckMaker();
+import java.util.ArrayList;
 
+public class BlackJackGame {
+    private Decks gameDeck;
+    private ArrayList<String> playerDeck, compDeck;
+    private int playerTotal, compTotal;
+
+    public BlackJackGame() {
+        gameDeck = new Decks();
+        playerDeck = new ArrayList<>();
+        compDeck = new ArrayList<>();
+        gameDeck.makeDeck();
+        System.out.println(gameDeck.showDeck()); //Testing
+        //playGame
+        //determineWinner
     }
     public static void main(String[] args) {
         BlackJackGame game = new BlackJackGame();
--- BlackJack/FinalBlackJack/Decks.java
+++ BlackJack/FinalBlackJack/Decks.java
@@ -4,22 +4,28 @@ import java.util.Collections;
 public class Decks {
     private ArrayList<String> cards;
 
-    public Decks() {
+    public Decks() { //Constructor
         cards = new ArrayList<>();
     }
-    public void deckMaker() {
+
+    public void makeDeck() { //Makes the deck
         String[] suitsArray = {"♠","♥","♦","♣"};
         String[] cardsArray = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
         for (int suit2 = 0; suit2<= 3; suit2++) {
             for (int card2 = 0; card2 <=12; card2++) {
-                System.out.println((cardsArray[card2]+suitsArray[suit2]));
+                cards.add(cardsArray[card2]+suitsArray[suit2]);
             }
         }
-    } //End of deckMaker
-    public void displayDeck() {
-        System.out.println(cards);
+        Collections.shuffle(cards);
+    } //End of makeDeck
+
+
+
+    //Testing Methods
+    public ArrayList showDeck() {
+        return cards;
     }
-}
+} //End of class
 /*
     //Deck Stuff
     private static String[] suitsArray = {"♠","♥","♦","♣"};
