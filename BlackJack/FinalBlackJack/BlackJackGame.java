8b5723bf40e81fad5224efd35727aa0f62cdf4f0
Devoid <42281396+Rubikscrafter@users.noreply.github.com>
2018-11-04 22:24:09 -0500

    Add files via upload

--- BlackJack/FinalBlackJack/BlackJackGame.java
+++ BlackJack/FinalBlackJack/BlackJackGame.java
@@ -1,40 +1,94 @@
 import java.util.ArrayList;
 
 public class BlackJackGame {
-    private Decks gameDeck;
-    private ArrayList<String> userDeck, compDeck;
-    private int userTotal, compTotal;
+  private Decks gameDeck;
+  private ArrayList<String> userDeck, compDeck;
+  private int userTotal, compTotal;
 
-    public BlackJackGame() {
-        gameDeck = new Decks();
-        userDeck = new ArrayList<>();
-        compDeck = new ArrayList<>();
-        gameDeck.makeDeck();
-        System.out.println(gameDeck.showDeck()); //Testing
-        playTheGame(); //Plays the game
-        determineWinner(); //Determines winner and gives win message
+  public BlackJackGame() {
+    gameDeck = new Decks();
+    userDeck = new ArrayList<>();
+    compDeck = new ArrayList<>();
+    gameDeck.makeDeck();
+    System.out.println("-------------------------------------------------------------------------------------------------------------------");
+    System.out.println("Welcome to Blackjack");
+    System.out.println("The goal is to get as close as you can to the number 21 without going over.");
+    System.out.println("Face cards are worth 10, and aces are worth either 1 or 11.");
+    playTheGame(); //Plays the game
+    determineWinner(); //Determines winner and gives win message
+  }
+
+  public void playTheGame() {
+    Scanner keys = new Scanner(System.in);
+    boolean game = true;
+    while (game) {
+      System.out.println("------");
+      System.out.println("Turn "+turn);
+      System.out.println("------");
+      System.out.println("Hit or Stand?");
+      choose = keys.nextLine();
+      while (!choose.equalsIgnoreCase("Hit") && !choose.equalsIgnoreCase("Stand")) {
+        System.out.println("Please enter a valid move");
+        choose = keys.nextLine();
+      }
+      choose = choose.toLowerCase();
+      switch (choose) {
+        case "hit":
+        takeTurn(0, playerTotal, turn); //Player Turn
+        takeTurn(1, compTotal, turn); //Computer Turn
+        case "stand":
+        break;
+      }
     }
-    public void playTheGame() {
-        boolean game = true;
-        while (game) {
-            for (int x = 0; x < 1; x++) {
-                turn(x);
-            }
+  }
+  public void takeTurn (int player, int score, int turn) {
+    int card;
+    if (turn == 1) {
+      
+    } else {
+      if (player == 0) {
+        card = drawCard();
+        if (score + card > 21) {
+          score += card;
+
         }
+      } else {
+        card = drawCard();
 
+      }
     }
-    public void turn (int player) {
-        if (player == 0) {
-            
-        } else {
+  }
 
-        }
+  public int drawCard() {
+    //String card = gameDeck.drawCard();
+    int cardVal = 0;
+    String value = gameDeck.drawCard().substring(0,drawnCard.length()-1);
+    if (!isNumeric(value)) {
+      if (value.equals("A")) {
+        cardVal = 1;
+      } else {
+        cardVal = 10;
+      }
+    } else {
+      cardVal = Integer.parseInt(value);
+    }
+    return cardVal;
+  }
 
-    }
-    public void determineWinner() {
+  public void determineWinner() {
 
+  }
+
+  private static boolean isNumeric(String strNum) {
+    try {
+      int d = Integer.parseInt(strNum);
+    } catch (NumberFormatException | NullPointerException nfe) {
+      return false;
     }
-    public static void main(String[] args) {
-        BlackJackGame game = new BlackJackGame();
-    }
+    return true;
+  } //End of isNumeric
+
+  public static void main(String[] args) {
+    BlackJackGame game = new BlackJackGame();
+  }
 }
--- BlackJack/FinalBlackJack/Decks.java
+++ BlackJack/FinalBlackJack/Decks.java
@@ -19,8 +19,8 @@ public class Decks {
         Collections.shuffle(cards);
     } //End of makeDeck
 
-    public void removeTopCard() {
-        cards.remove(0);
+    public String drawCard() {
+        return cards.remove(0);
     }
 
 
@@ -29,3 +29,26 @@ public class Decks {
         return cards;
     }
 } //End of class
+/*
+    //Deck Stuff
+    private static String[] suitsArray = {"♠","♥","♦","♣"};
+    private static ArrayList<String> suits = new ArrayList<String>(); //Suits
+
+    private static ArrayList<String> cards = new ArrayList<String>(); //Cards
+    private static ArrayList<String> dealerDeck = new ArrayList<String>(); //Deck
+    //Game Deck Stuff
+    private static ArrayList<String> playerHand = new ArrayList<String>(); //Player's Drawn Cards
+    private static ArrayList<Boolean> aceCheck = new ArrayList<Boolean>(); //Checks for drawn aces
+    private static ArrayList<Boolean> usedAceCheck = new ArrayList<Boolean>(); //Checks for aces accounted for
+    //Creating the deck
+    private static void createDeck() {
+        Collections.addAll(suits,suitsArray);
+        Collections.addAll(cards,cardsArray);
+        for (int suit = 0; suit<= 3; suit++) {
+            for (int card = 0; card <=12;card++) {
+                dealerDeck.add(cards.get(card)+suits.get(suit));
+            }
+        }
+        cardPick = rng.nextInt(dealerDeck.size());
+    }
+    */
