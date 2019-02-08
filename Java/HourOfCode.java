1a2df4e73c7796fb0e9c04580a66dd732dfaa808
Devoid <42281396+Rubikscrafter@users.noreply.github.com>
2019-02-07 23:18:01 -0500

    Add files via upload

---
+++ Java/HourOfCode.java
@@ -0,0 +1,11 @@
+public class HourOfCode {
+  public static void main (String [] args) {
+    System.out.println("Calculator");
+    double x = 5.5;
+    double y = x/2;
+    String z = "This is a string";
+    System.out.println(x);
+    System.out.println(y);
+    System.out.println(z);
+  }
+}
