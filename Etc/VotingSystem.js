8af64f24cd55e4cfb28081e24284da36e1de5c68
Devoid <42281396+Rubikscrafter@users.noreply.github.com>
2019-02-23 15:00:13 -0500

    Add files via upload

--- Etc/VotingSystem.js
+++ Etc/VotingSystem.js
@@ -4,6 +4,7 @@ var sheet;
 var ui = SpreadsheetApp.getUi();
 pickSheet().getRange(1, 1, sheet.getMaxRows(), sheet.getMaxColumns()).setWrap(true);
 
+
 // Sets the active sheet
 function pickSheet() {
   sheet = SpreadsheetApp.getActiveSheet();
@@ -17,18 +18,31 @@ function isAuthenticated() {
 }
 
 
+//--------------------------------------------------------------------------------------------------------------------------//
+//---------------------------------------------------/Ribbon Menus/---------------------------------------------------------//
+//--------------------------------------------------------------------------------------------------------------------------//
+
+
+// Runs when a person opens the spreadsheet
 function onOpen() {
   // Creates the custom dropdown menu on the ribbon
   if (isAuthenticated()) {
-    ui.createMenu('Voting System')
+    ui.createMenu('Voting System') //---------------------/Change to ui.createAddonMenu() when publishing as add-on/--------//
     .addItem('Create Sheet Headers','createHeaders')
-    .addItem('Make Voting Cells','makeVotingCells')
+    .addItem('Make New Sheet with Same Headers', 'duplicateSheetAndHeaders')
     .addSeparator()
-    .addItem('Clear Sheet Headers','clearHeaders')
-    .addItem('Resize Header Columns','resizeHeaders')
+    .addItem('Clear Header Row','clearHeaders')
+    .addItem('Change All Column Widths','setWidths')
+    .addSeparator()
+    .addItem('Change Header Widths','resizeHeaders')
     .addItem('Change Header Font Size', 'changeHeaderFontSize')
     .addSeparator()
+    .addItem('Make Voting Cells','makeVotingCells')
+    .addSeparator()
     .addItem('Info', 'info')
+    .addSeparator()
+    .addItem('Del Sheets','delSheets')
+    .addItem('Del Rows', 'delRows')
     .addToUi();
   }
   ui.createMenu('Vote')
@@ -36,11 +50,18 @@ function onOpen() {
   .addToUi()
 }
 
-if (isAuthenticated()) { // Only gives access to Soder and me (so I can test)
-  // Gives the data of the header row
-  function getHeaders () {
-    return sheet.getSheetValues(1, 1, 1, sheet.getMaxColumns());
-  }
+
+//--------------------------------------------------------------------------------------------------------------------------//
+//------------------------------------------------/Voting System Options/---------------------------------------------------//
+//--------------------------------------------------------------------------------------------------------------------------//
+
+
+if (isAuthenticated()) { // Only gives access to the creation of the voting system to the owner of the spreadsheet
+
+
+  //--------------------------------------------------------------------------------------------------------------------------//
+  //------------------------------------------------/First Options Section/---------------------------------------------------//
+  //--------------------------------------------------------------------------------------------------------------------------//
 
 
   // Creates the headers
@@ -76,84 +97,83 @@ if (isAuthenticated()) { // Only gives access to Soder and me (so I can test)
   }
 
 
-  // Aligns the header row to what the user inputs
-  function headerAlign() {
-    var position = ui.prompt('How do you want the headers aligned?', '(left/center/right)', ui.ButtonSet.OK);
-    if (position.getSelectedButton() == ui.Button.OK) {
+    // Creates a new sheet with the same headers as the active sheet
+  function duplicateSheetAndHeaders() {
+    var resp = ui.prompt('How many sheets to you want to make?', ui.ButtonSet.OK);
+    if (resp.getSelectedButton() == ui.Button.OK) {
       while (true) {
-        var pos = position.getResponseText();
-        pos = pos.toLowerCase();
-        var range = sheet.getRange(1,1,1,sheet.getMaxColumns());
-        if (pos == 'left' || pos == 'center' || pos == 'right') {
-          var align = [[]];
-          for (x = 0; x < sheet.getMaxColumns(); x++) {
-            align[0].push(pos);
+        var text = resp.getResponseText();
+        if (text != '') {
+          if (!isNaN(parseInt(text))) {
+            for (num = 0; num < parseInt(text); num++) {
+              // Gets the header data from the sheet to duplicate
+              var headers = getTextHeaders();
+              var headWidth = sheet.getColumnWidth(1);
+              var headAlign = sheet.getRange(1,1).getHorizontalAlignment();
+              var headSize = sheet.getRange(1,1).getFontSize();
+
+              // Makes the new sheet and makes it active
+              ss.insertSheet();
+              sheet = pickSheet();
+
+              // Sets the header row to match the header row of the initial sheet (w/o 'Voting' and empty spaces)
+              sheet.setFrozenRows(1);
+              var range = sheet.getRange(1,1,1, headers[0].length);
+              range.setValues(headers);  // Header text
+              sheet.setColumnWidths(1, sheet.getMaxColumns(), headWidth);  // Width
+              sheet.getRange(1,1,1, sheet.getMaxColumns()).setHorizontalAlignment(headAlign);  // Alignment
+              sheet.getRange(1,1,1, sheet.getMaxColumns()).setFontSize(headSize);  // Font size
+            }
+            break;
+          } else {
+            ui.alert('Please enter an integer number of sheets to create.');
+            resp = ui.prompt('How many sheets to you want to make?', ui.ButtonSet.OK);
           }
-          range.setHorizontalAlignments(align);
+        } else
           break;
-        } else if (pos == ''){
-          break;
-        } else {
-          ui.alert('Error: Please enter \'left\', \'center\', or \'right\'');
-          position = ui.prompt('How do you want the headings aligned?', '(left,center,right)', ui.ButtonSet.OK);
-        }
       }
-      setWidths();
     }
   }
 
 
-  // Sets the size of all the columns that have a header over them to be a certain number of pixels
-  function setWidths() {
-    var width = columnWidth(sheet.getColumnWidth(1));
-    sheet.setColumnWidths(1, sheet.getMaxColumns(), width);
-  }
-
-
-  // Input box for the column width
-  function columnWidth(initialWidth) {
-    var width = ui.prompt(
-      'How many pixels wide do you want the columns to be?',
-      '(Recommended: 250)',
-      ui.ButtonSet.OK);
-    if (width.getSelectedButton() == ui.Button.OK) { // If user clicks OK
-      while (true) {
-        var resp = width.getResponseText();
-        if (resp != "") {
-          if (isNaN(parseInt(resp))){  // If you entered a string
-            ui.alert('Error: Please enter a number of pixels for the width of the columns.');
-            width = ui.prompt(
-              'How many pixels wide do you want the columns to be?',
-              '(Recommended: 250)',
-              ui.ButtonSet.OK);
-          } else
-            return parseInt(resp);
-        } else
-          return initialWidth;
-      }
-    } else // If user clicks out of the prompt with x
-      return initialWidth;
-  }
+  //--------------------------------------------------------------------------------------------------------------------------//
+  //------------------------------------------------/Second Options Section/--------------------------------------------------//
+  //--------------------------------------------------------------------------------------------------------------------------//
 
 
   // Makes the header row blank
   function clearHeaders() {
-    var answer = ui.alert('Are you sure you want to delete ALL headers?', ui.ButtonSet.YES_NO);
+    var answer = ui.alert('Are you sure you want to delete ALL of the data on the header row?', ui.ButtonSet.YES_NO);
     if (answer == ui.Button.YES)
       sheet.getRange(1,1,1, sheet.getMaxColumns()).clear();
   }
+
+
+    // Sets the size of all the columns in the entire sheet
+  function setWidths() {
+    var width = columnWidth(sheet.getColumnWidth(1));
+    if (width != 0)
+      sheet.setColumnWidths(1, sheet.getMaxColumns(), width);
+  }
+
+
+  //--------------------------------------------------------------------------------------------------------------------------//
+  //-------------------------------------------------/Third Options Section/--------------------------------------------------//
+  //--------------------------------------------------------------------------------------------------------------------------//
 
 
   // Resizes the headers that are not blank or for voting
   function resizeHeaders() {
     var columnCounter = 1;
     var width = columnWidth(sheet.getColumnWidth(1));
-    var headers = getHeaders();
-    headers[0].forEach(function(header) {
-      if (header != '' && header != 'Voting')
-        sheet.setColumnWidth(columnCounter, width);
-      columnCounter++;
-    });
+    if (width != 0) {
+      var headers = getHeaders();
+      headers[0].forEach(function(header) {
+        if (header != '' && header != 'Voting')
+          sheet.setColumnWidth(columnCounter, width);
+        columnCounter++;
+      });
+    }
   }
 
 
@@ -172,50 +192,52 @@ if (isAuthenticated()) { // Only gives access to Soder and me (so I can test)
   }
 
 
+  //--------------------------------------------------------------------------------------------------------------------------//
+  //------------------------------------------------/Fourth Options Section/--------------------------------------------------//
+  //--------------------------------------------------------------------------------------------------------------------------//
+
+
   // Makes the upvote and downvote columns
   function makeVotingCells() {
-    var columnCounter = 2;
+    var columnCounter = 1;
     var headers = getHeaders();
     headers[0].forEach(function(header) {
+      // If the header is neither empty nor has 'Voting' in the box
       if (header != '' && header != 'Voting') {
-        var responseRows = getResponseRowAmnt(columnCounter-1);
-        if (responseRows != 0) {
-          for (i = 0; i < 3; i++)
-            sheet.insertColumnAfter(columnCounter-1);
-          sheet.getRange(1,columnCounter, 1, 2).merge();
-          sheet.getRange(1,columnCounter, 1, 1).setValue('Voting').setHorizontalAlignment('center');
+        var responseRows = getResponseRowAmnt(columnCounter);
+        // If there is something under the header
+        if (responseRows.length != 0) {
 
-          for (row = 2; row <= responseRows; row++) {
-            sheet.getRange(row,columnCounter, 1, 1).setValue('=IMAGE("http://i67.tinypic.com/zwcexc.jpg", 4, 20, 20)').setHorizontalAlignment("center").setVerticalAlignment("middle"); // Upvote
-            sheet.getRange(row,columnCounter+1, 1, 1).setValue('=IMAGE("http://i67.tinypic.com/23vi253.jpg", 4, 20, 20)').setHorizontalAlignment("center").setVerticalAlignment("middle"); // Downvote
+          // Makes the three voting columns
+          for (i = 0; i < 3; i++)
+            sheet.insertColumnAfter(columnCounter);
+
+          // Sets the values in the voting columns
+          sheet.getRange(1, columnCounter+1, 1, 2).merge();
+          sheet.getRange(1, columnCounter+1).setValue('Voting').setHorizontalAlignment('center');
+          sheet.getRange(1, columnCounter+3, responseRows[responseRows.length-1]).setBackground('grey');
+          sheet.setColumnWidth(columnCounter+3, 5);
+
+          // Puts the voting columns next to the cells that are filled
+          for (len = 0; len < responseRows.length; len++) {
+            var rowPlacer = responseRows[len];
+            sheet.getRange(rowPlacer, columnCounter + 1).setValue('=IMAGE("http://i67.tinypic.com/zwcexc.jpg", 4, 20, 20)').setHorizontalAlignment("center").setVerticalAlignment("middle"); // Upvote
+            sheet.getRange(rowPlacer, columnCounter + 2).setValue('=IMAGE("http://i67.tinypic.com/23vi253.jpg", 4, 20, 20)').setHorizontalAlignment("center").setVerticalAlignment("middle"); // Downvote
+            sheet.setRowHeight(rowPlacer, 30);
           }
-          sheet.setColumnWidths(columnCounter, 2, 30);
-          sheet.setRowHeights(2, responseRows, 30);
-          sheet.setColumnWidth(columnCounter+2, 5);
-          sheet.getRange(1, columnCounter+2, responseRows).setBackground('grey');
+          sheet.setColumnWidths(columnCounter + 1, 2, 30);
+          sheet.setColumnWidth(columnCounter + 3, 5);
           columnCounter += 3;
         }
-      }
+      } // Close voting or empty if
       columnCounter++;
     });
   }
 
 
-  // Returns the number of rows that have responses in them
-  function getResponseRowAmnt(column) {
-    var rows = 1;
-    var numRows = 2;
-    var col = sheet.getRange(numRows, column).getValue();
-    if (col != '') {
-      while (col != '') {
-        numRows++;
-        rows++;
-        col = sheet.getRange(numRows, column).getValue();
-      }
-      return rows;
-    } else
-      return 0;
-  }
+  //--------------------------------------------------------------------------------------------------------------------------//
+  //------------------------------------------------------/Info/--------------------------------------------------------------//
+  //--------------------------------------------------------------------------------------------------------------------------//
 
 
   // Gives info to Soder about how it works
@@ -235,14 +257,120 @@ if (isAuthenticated()) { // Only gives access to Soder and me (so I can test)
              '- Danilo Montes'
             );
   }
-}
 
 
+  //--------------------------------------------------------------------------------------------------------------------------//
+  //-----------------------------------------------------/Functions/----------------------------------------------------------//
+  //--------------------------------------------------------------------------------------------------------------------------//
+
+
+  // Aligns the entire header row to what the user inputs
+  function headerAlign() {
+    var position = ui.prompt('How do you want the headers aligned?', '(left/center/right)', ui.ButtonSet.OK);
+    if (position.getSelectedButton() == ui.Button.OK) {
+      while (true) {
+        var pos = position.getResponseText();
+        if (pos.toLowerCase() == 'left' || pos.toLowerCase() == 'center' || pos.toLowerCase() == 'right') {
+          sheet.getRange(1,1,1,sheet.getMaxColumns()).setHorizontalAlignment(pos);
+          break;
+        } else if (pos == ''){
+          break;
+        } else {
+          ui.alert('Error: Please enter \'left\', \'center\', or \'right\'');
+          position = ui.prompt('How do you want the headings aligned?', '(left,center,right)', ui.ButtonSet.OK);
+        }
+      }
+      setWidths();
+    }
+  }
+
+
+  // Input box for the column width
+  function columnWidth(initialWidth) {
+    var width = ui.prompt(
+      'How many pixels wide do you want the columns to be?',
+      '(Recommended: 250)',
+      ui.ButtonSet.OK);
+    if (width.getSelectedButton() == ui.Button.OK) { // If user clicks OK
+      while (true) {
+        var resp = width.getResponseText();
+        if (resp != '') {
+          if (isNaN(parseInt(resp))){  // If you entered a string
+            ui.alert('Error: Please enter an integer number of pixels for the width of the columns.');
+            width = ui.prompt(
+              'How many pixels wide do you want the columns to be?',
+              '(Recommended: 250)',
+              ui.ButtonSet.OK);
+          } else
+            return parseInt(resp);
+        }
+        break;
+      }
+    }
+    return 0;
+  }
+
+
+  // Marks the row numbers that have responses in them and returns an array of the row numbers
+  function getResponseRowAmnt(column) {
+    var rowSelector = [];
+    for (row = 2; row <= sheet.getMaxRows(); row++) {
+      var specRow = sheet.getRange(row, column).getValue();
+      if (specRow != '')
+        rowSelector.push(row);
+    }
+    return rowSelector;
+  }
+
+
+  // Gives the data of the header row
+  function getHeaders() {
+    return sheet.getSheetValues(1, 1, 1, sheet.getMaxColumns());
+  }
+
+
+  // Gives the data of the header row that is neither empty nor 'Voting'
+  function getTextHeaders() {
+    var textHeaders = [[]];
+    var head = getHeaders();
+    head[0].forEach(function(header) {
+      if (header != '' && header != 'Voting')
+        textHeaders[0].push(header);
+    });
+    return textHeaders;
+  }
+
+
+  //--------------------------------------------------------------------------------------------------------------------------//
+  //-----------------------------------------------------/Temporary/----------------------------------------------------------//
+  //--------------------------------------------------------------------------------------------------------------------------//
+
+
+  // Deletes sheets in the given number range
+  function delSheets() {
+    var resp = parseInt(ui.prompt('Number of first sheets to keep').getResponseText());
+    var sheets = ss.getSheets();
+    for (she = resp; she < sheets.length; she++)
+      ss.deleteSheet(sheets[she]);
+  }
+
+
+  // Deletes unnecessary rows
+  function delRows() {
+    var resp = parseInt(ui.prompt('Last row to be kept').getResponseText());
+    sheet.deleteRows(resp, sheet.getMaxRows()-resp);
+  }
+
+} // End of Voting System Options
+
+
+//--------------------------------------------------------------------------------------------------------------------------//
+//--------------------------------------------------------/Vote/------------------------------------------------------------//
+//--------------------------------------------------------------------------------------------------------------------------//
 
 
 // Everyone can access this part
 // Vote based on what arrow you have selected
-/*highlight.offset(0,-1).getFontSize() + 1*/
 function vote() {
   var highlight = SpreadsheetApp.getCurrentCell();
   if (highlight.getFormula() == '=IMAGE("http://i67.tinypic.com/zwcexc.jpg", 4, 20, 20)') { // If upvote
