3d6735f1c673c0c2ec847d3be22bc50bd629b390
Devoid <Rubikscrafter@users.noreply.github.com>
2019-08-11 01:23:45 -0400

    Fixed a check

--- Python/Udemy Course/FinalCapstoneProjects/BinaryToDec.py
+++ Python/Udemy Course/FinalCapstoneProjects/BinaryToDec.py
@@ -1,26 +1,28 @@
-def int_checker(inp):
-    while int(inp) is NaN:
-        print('Error: Please enter a numerical answer\n')
-        inp = input()
-    return inp
-
-
 def convertToDec(number):
     pass
+
 
 def convertToBin(number):
     pass
 
+
 def main():
     print('Base Converter\n')
 
-    change = input('What base would you like to convert from and to? (1 - Binary to Decimal or 2 - vise versa)\n')
+    while True:
+        try:
+            change = \
+                int(input('What base would you like to convert from and to? (1 - Binary to Decimal or 2 - vise versa)\n'))
+        except ValueError:
+            print('Error: Please enter an integer')
+        else:
+            break
 
-    while int_checker(change) is not 1 or 2:
+    while change is not 1 or 2:
         print('Error: Please enter either 1 or 2')
         change = input('What base would you like to convert from and to? (1 - Binary to Decimal or 2 - vise versa)\n')
 
-    num = int_checker(input('What number are you converting?\n'))
+    num = input('What number are you converting?\n')
 
     if change == 1:
         convertToDec(num)
