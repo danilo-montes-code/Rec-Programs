4339827bad150c32c26b4e00d120dc94b37f3df6
Devoid <realrubikscrafter@gmail.com>
2020-03-25 15:39:25 -0400

    d

--- Python/Scripts/Short Scripts/alphabetize.py
+++ Python/Scripts/Short Scripts/alphabetize.py
@@ -1,20 +1,4 @@
-list = ['Priyal Garg- priyalgarg@gmail.com',
-'Hayagriv Gopinath - hgopinath@gmail.com',
-'Serena Huang - serenah678@gmail.com',
-'Winston Huang - winstonhuang787@gmail.com',
-'Mansi Kumar - kbmansi@gmail.com',
-'Alice Lee -alice.lee5018@gmail.com',
-'Jackson Lee - Mabajack@gmail.com',
-'Ryan Lin - ryanlin02@gmail.com',
-'Ilyas Mehkri- imehkri@pluscompt.net',
-'Steven Mi-   stevenm0208@gmail.com ',
-'Danilo Montes -dmontes@ebnet.org',
-'Laura Muyingo- lmuyingo@ebnet.org or lauramuyingo@gmail.com ',
-'Lily Sheng - lsheng2@ebnet.org',
-'John Wang - wangjohn468@gmail.com',
-'Kyle Wang - kylewang239@gmail.com',
-'Eric Xu - ericxu54321@gmail.com',
-'Millie Trusler - Mtrusler@ebnet.org']
+list = []
 list.sort()
 
 for i in list:
--- Python/Scripts/Short Scripts/combine_first_and_last.py
+++ Python/Scripts/Short Scripts/combine_first_and_last.py
@@ -1,4 +1,4 @@
-all = 'Alex	Lee Belinda	Zhang Jeffrey	Han Joshua	Aalampour Joy	Chen Leah	Chin Mary	Fouad Pranav	Nair Raveesh	Mehta Roey	Beniluz Ryan	Lin Samiyah	Siddiqui Sanchita	Ray Shreya	Sinkar Aaron	Zhu Aniket	Das Anthony	Leapo Ayman	Mohammad Benjamin	Hong Bhaskar	Jain Brandon	Gao Bryan	Lee Caelie	Moi Chinmay	Sahasrabudhe Christine	Chang Clara	Lee Edwin	Chao Elizabeth	Mundkowsky Emily	Chu Erik	Wei Erin	Ma Fiona	Liu Flora	Cai Harrison	Wong Horace	Yu Jessica	Chou Jhanvi	Pai Joshua	Yi Kaitlyn	Liu Kavin	Mohan Klara	Tosic Lara	Petrunis Michael	Mogilevsky Michael	Sun Minta	Caune Neha	Ravuri Rachel	Ai Rothela	Samadi Sandra	Lee Sarah	Levin Sophia 	Jorgensen Srinu	Appana Steven	Han Tarun	Sivakumar Tracy	Wan Varun	Mehrotra Yoshna	Rajendran Yusuf	Zahran'
+all = ''
 all = all.split()
 combine = []
 temp = ''
