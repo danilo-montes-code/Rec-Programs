5c578703f6d67bc68d3d4c509919d228260e0f0f
danilo-montes <dem9855@nyu.edu>
2023-03-23 14:19:51 -0400

    Add gitignore, README, documentation
    Added:
    * README.md
    * .gitignore
    
    minecraft-villager-trades.py:
    * added function and script documentation

--- Python/Scripts/Short Scripts/get_villager_data/README.md
+++ Python/Scripts/Short Scripts/get_villager_data/README.md
@@ -1,3 +1,12 @@
 # get_villager_data
 
 Scrapes https://minecraft.fandom.com/wiki/Trading to get the villager trade information and saves the data to a file so I don't have to open the wiki 100 times a day.
+
+## Features
+* access https://minecraft.fandom.com/wiki/Trading
+* write data to file
+* read data from file
+
+## Planned Features
+* command line arguments for getting only given professions
+* different display options (simple, complex, full)
--- Python/Scripts/Short Scripts/get_villager_data/minecraft-villager-trades.py
+++ Python/Scripts/Short Scripts/get_villager_data/minecraft-villager-trades.py
@@ -1,16 +1,56 @@
+"""minecraft-villager-trades
+
+This script connects to the Minecraft wiki, parses the villager trade
+information, and saves it to a file for offline viewing.
+
+If there is a file already present, it will simply read from the file.
+Otherwise, it will connect to the website.
+
+Dependencies:
+* requests
+* BeautifulSoup
+"""
+
 import requests
 from bs4 import BeautifulSoup
 
+#################################################
+#                     Main                      #
+#################################################
 
-def main():
-    dom = connect()
 
-    prof_list = get_list(dom)
 
-    display(prof_list)
+def main() -> None:
+    """driver function"""
+
+
+    try: 
+        data = open('villager-data.txt', 'r')
+
+        data.close()
+
+    except:
+        # file does not exist, connect to wiki
+        dom = connect()
+
+        prof_list = get_list(dom)
+
+        display(prof_list)
     
 
-def connect():
+#################################################
+#              Connecting and DOM               #
+#################################################
+
+def connect() -> BeautifulSoup:
+    """Connects to the Minecraft Wiki Trading page
+
+    Returns
+    -------
+    BeautifulSoup
+        a BeautifulSoup object representing the DOM of the website
+    """
+
     URL = "https://minecraft.fandom.com/wiki/Trading"
     page = requests.get(URL)
 
@@ -19,30 +59,41 @@ def connect():
     return soup
 
 
-def get_list(dom):
-    pass
+def get_list(dom) -> list:
+    """Parses the DOM to get the required items to put in a list
+
+    Parameters
+    ----------
+    dom : BeautifulSoup
+        the DOM of the website
+
+    Returns
+    -------
+    list
+        a list of DOM objects for saving
+    """
+
+    return []
 
 
 '''
-[
-    {
-        "profession" : <PROFESSION>,
-        "trades" : [
-            {
-                "level" : <LEVEL>,
-                "exchanges" : [
-                    {
-                        "wanted" : <ITEM>,
-                        "given"  : <ITEM>
-                    },
-                    ...
-                ]
-            },
-            ...
-        ]  
-    },
-    ...
-]
+JSON format of villager records
+{
+    "profession" : <PROFESSION>,
+    "trades" : [
+        {
+            "level" : <LEVEL>,
+            "exchanges" : [
+                {
+                    "wanted" : <ITEM>,
+                    "given"  : <ITEM>
+                },
+                ...
+            ]
+        },
+        ...
+    ]  
+}
 '''
 
 def display(data):
