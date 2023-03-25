d389c0025e84e29e55ddedc4cb4c21ff90245a3a
danilo-montes <dem9855@nyu.edu>
2023-03-24 21:33:48 -0400

    got professions, job site blocks, and tables

--- Python/Scripts/Short Scripts/get_villager_data/minecraft-villager-trades.py
+++ Python/Scripts/Short Scripts/get_villager_data/minecraft-villager-trades.py
@@ -11,40 +11,54 @@ Dependencies:
 * BeautifulSoup
 """
 
-import requests, json, os.path
+import requests, json, os.path, sys
 from typing import TextIO
 from bs4 import BeautifulSoup
+
+
+
+TESTING = True
+
+
+
 
 #################################################
 #                     Main                      #
 #################################################
 
-
-
 def main() -> None:
     """driver function"""
 
-    # if file does not exist
-    if not os.path.isfile('villager-data.txt'):
-        if not create_file():
+    if not TESTING: 
+        # if file does not exist
+        if not os.path.isfile('villager-data.txt'):
+            if not create_file():
+                return
+
+            with open('villager-data.txt', 'w') as f:
+                dom = connect()
+                prof_list = get_list(dom)
+                data = make_into_json(prof_list)
+                write_to_file(f, data)
+
+
+        # open file
+        file = open_file()
+
+        if file is None:
             return
+        
+        # display data from file
+        # TODO add different display options
+        # TODO add command line args
+        display_data(data)
 
-        with open('villager-data.txt', 'w') as f:
-            dom = connect()
-            prof_list = get_list(dom)
-            data = make_into_json(prof_list)
-            write_to_file(f, data)
+    else:
 
-    # open file
-    file = open_file()
-
-    if file is None:
-        return
-    
-    # display data from file
-    # TODO add different display options
-    # TODO add command line args
-
+        dom = connect()
+        prof_list = get_list(dom)
+        data = make_into_json(prof_list)
+        display_data(data)
     
 
 
@@ -119,7 +133,7 @@ def connect() -> BeautifulSoup:
     return soup
 
 
-def get_list(dom) -> list:
+def get_list(dom: BeautifulSoup) -> list:
     """Parses the DOM to get the required items to put in a list
 
     Parameters
@@ -133,6 +147,69 @@ def get_list(dom) -> list:
         a list of DOM objects
     """
 
+    ##### This wiki is *very* annoyingly formatted so parsing isn't as
+    ##### simple as it should be
+
+
+    # get headers related to villager professions
+    headers = dom.select('h3')
+    headers = headers[3:16]
+
+    professions = []
+    for header in headers:
+        professions.append(header.get_text().lower())
+
+    
+    # get job sites for each profession
+    job_sites_span = dom.select('h3 ~ p > a[href^="/wiki/"] > span > span.sprite-text')
+
+    job_sites = []
+    for job in job_sites_span:
+        job_sites.append(job.get_text().lower())
+
+    job_sites = job_sites[:13]
+
+
+
+
+    # for i, prof in enumerate(professions):
+    #     print(str(i) + ': ' + prof)
+
+    # for i in range(13):
+    #     print(professions[i] + ' -> ' + job_sites[i])
+
+
+
+    # get tables related to villager trades
+    tables = dom.select('h3 + p + figure + table.wikitable')
+
+    # get mason table (due to extra note making it not appear before)
+    mason_table = dom.select_one('h3 + p + figure + div + table.wikitable')
+    tables = tables[:13]
+
+    # shift array down and place mason in right position
+    for i in range(len(tables) - 1, 8, -1):
+        tables[i] = tables[i-1]
+    tables[9] = mason_table
+
+
+
+
+
+
+
+
+    with open(os.path.join(sys.path[0], 'tables.txt'), 'w') as f:
+        
+        stuff = []
+        for table in tables:
+            desc = table['data-description']
+            parts = desc.split(' ')
+
+            stuff.append(desc)
+
+        f.write('\n'.join(stuff))
+
     return []
 
 
@@ -144,30 +221,41 @@ def get_list(dom) -> list:
 JSON format of villager records
 {
     "profession" : <PROFESSION>,
+    'job-site-block' : <JOB SITE BLOCK>,
     "trades" : [
         {
             "level" : <LEVEL>,
             "exchanges" : [
                 {
-                    "wanted" : <ITEM>,
-                    "given"  : <ITEM>
+                    "wanted" : {
+                        'item' : <ITEM>,
+                        'default-quantity' : <NUMBER>,
+                        'price-multiplier' : <NUMBER>
+                    },
+                    "given"  : {
+                        'item' : <ITEM>,
+                        'quantity' : <NUMBER>,
+                    },
+                    'trades-until-disabled' : <NUMBER>,
+                    'xp-to-villager' : <NUMBER>
                 },
                 ...
             ]
         },
         ...
-    ]  
+    ]\
 }
 '''
 def make_into_json(data):
-    for item in data:
-        print('[=========' + item['profession'] + '=========]')
+    # for item in data:
+    #     print('[=========' + item['profession'] + '=========]')
 
-        for trade in item['trades']:
-            print('----' + trade['level'] + '----')
+    #     for trade in item['trades']:
+    #         print('----' + trade['level'] + '----')
 
-            for exchange in item['trades']['exchanges']:
-                pass
+    #         for exchange in item['trades']['exchanges']:
+    #             pass
+    return ['hi']
 
 
 
@@ -177,14 +265,15 @@ def make_into_json(data):
 #################################################
 
 def display_data(villagers):
-    for item in villagers:
-        print('[=========' + item['profession'] + '=========]')
+    # for item in villagers:
+    #     print('[=========' + item['profession'] + '=========]')
 
-        for trade in item['trades']:
-            print('----' + trade['level'] + '----')
+    #     for trade in item['trades']:
+    #         print('----' + trade['level'] + '----')
 
-            for exchange in item['trades']['exchanges']:
-                print(exchange)
+    #         for exchange in item['trades']['exchanges']:
+    #             print(exchange)
+    print(villagers[0])
 
 
 
